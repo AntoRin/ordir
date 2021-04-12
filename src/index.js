0a88d9818de7ab907dad7d265c63d2d211ebe96f
AntoRin_ux <antorinish@gmail.com>
2021-04-12 13:01:18 +0530

    Update version of the package

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const initialize = require("./init");
 const { showHelp } = require("./utils/help");
 const { handleError } = require("./utils/errorHandler");
 
-const version = "1.2.1";
+const version = "1.3.0";
 
 function beginInterface() {
    const allFlags = [
